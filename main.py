ca04f0faee099147df97a3a1f3b764e7966ec4f1
FuXE <73062901+p-arefiev@users.noreply.github.com>
2021-11-18 22:48:33 +0100

    added: busters to global & support redteam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import random
 
 # Send your busters out into the fog to trap ghosts and bring them home!
 
@@ -9,7 +10,6 @@ my_team_id = int(
     input()
 )  # if this is 0, your base is on the top left of the map, if it is one, on the bottom right
 
-
 class Entity:
     def __init__(self, entity_id, x, y, state, value):
         self.entity_id = entity_id
@@ -17,7 +17,6 @@ class Entity:
         self.y = y
         self.state = state
         self.value = value
-
 
 class Buster(Entity):
     def __init__(self, entity_id, x, y, entity_type, state, value):
@@ -28,14 +27,12 @@ class Buster(Entity):
             state,
             value,
         )
-
         self.entity_type = entity_type
         self.closest_ghost = 0
         self.closest_ghost_dist = 0
         self.closest_ghost_x = 0
         self.closest_ghost_y = 0
         self.status = "IDLE"
-
 
 class Ghost(Entity):
     def __init__(self, entity_id, x, y, entity_type, state, value):
@@ -46,25 +43,43 @@ class Ghost(Entity):
             state,
             value,
         )
-
         self.entity_type = entity_type
 
+
+all_buster = []
+for i in range(busters_per_player):
+    if my_team_id == 0:
+        buster = Buster(i, 0, 0, 0, 0, 0)
+        all_buster.append(buster)
+    else:
+        buster = Buster(i + busters_per_player, 0, 0, 1, 0, 0)
+        all_buster.append(buster)
+
+ori_x = 0
+ori_y = 0
+base_x = 1600
+base_y = 1600
+if my_team_id == 1:
+    ori_x = 16000
+    ori_y = 9000
+    base_x = ori_x - 1600
+    base_y = ori_y - 1600
 
 def info_turn(entity_id, x, y, entity_type, state, value):
     if entity_type == -1:
         ghost = Ghost(entity_id, x, y, entity_type, state, value)
         all_ghost.append(ghost)
-
-    elif entity_type == my_team_id:
-        buster = Buster(entity_id, x, y, entity_type, state, value)
-        all_buster.append(buster)
-
+    elif entity_type == my_team_id :
+            n = entity_id if my_team_id == 0 else entity_id - busters_per_player
+            all_buster[n].x = x
+            all_buster[n].y = y
+            all_buster[n].state = state
+            all_buster[n].value = value
 
 def distance(x1, y1, x2, y2):
     return int(math.sqrt(math.pow((x1 - x2), 2) + math.pow((y1 - y2), 2)))
 
-
-def closest_busters(all_ghost, all_buster):
+def closest_busters(all_ghost):
 
     for ghost_iter in range(len(all_ghost)):
         buster_dist = []
@@ -76,46 +91,55 @@ def closest_busters(all_ghost, all_buster):
                 all_ghost[ghost_iter].x,
                 all_ghost[ghost_iter].y,
             ))
-
         #print(f"min dist : {min_buster}", file=sys.stderr, flush=True)
         #print(f"buster dist : {buster_dist}", file=sys.stderr, flush=True)
         min_buster = min(buster_dist)
         min_index = buster_dist.index(min_buster)
-        if all_buster[min_index].status == "IDLE":
-            all_buster[min_index].closest_ghost = all_ghost[ghost_iter].entity_id
-            all_buster[min_index].closest_ghost_x = all_ghost[ghost_iter].x
-            all_buster[min_index].closest_ghost_y = all_ghost[ghost_iter].y
-            all_buster[min_index].closest_ghost_dist = min_buster
+        all_buster[min_index].closest_ghost = all_ghost[ghost_iter].entity_id
+        all_buster[min_index].closest_ghost_x = all_ghost[ghost_iter].x
+        all_buster[min_index].closest_ghost_y = all_ghost[ghost_iter].y
+        all_buster[min_index].closest_ghost_dist = min_buster
 
-def update_status(all_buster):
+def in_base(x, y):
+    if my_team_id == 1:
+        if x < base_x or y < base_y:
+            return False
+        else:
+            return True
+    else:
+        if x > base_x or y > base_y:
+            return False
+        else:
+            return True
+
+def update_status():
     for buster_iter in all_buster:
-
-        if buster_iter.value != -1 and (buster_iter.x > 1600 or buster_iter.y > 1600) :
+        if buster_iter.value != -1 and not in_base(buster_iter.x, buster_iter.y) :
             buster_iter.status = "GB"
             continue
-        elif buster_iter.value != -1 and (buster_iter.x <= 1600 or buster_iter.y <= 1600):
+        elif buster_iter.value != -1 and in_base(buster_iter.x, buster_iter.y):
             buster_iter.status = "READY"
+            buster_iter.closest_ghost = 0
             continue
         else:
             pass
-
         if buster_iter.closest_ghost > 0 and buster_iter.closest_ghost_dist > 1700:
             buster_iter.status = "CHASING"
         elif buster_iter.closest_ghost > 0 and buster_iter.closest_ghost_dist < 1700 and buster_iter.closest_ghost_dist > 900:
             buster_iter.status = "BUSTING"
         elif buster_iter.closest_ghost > 0 and buster_iter.closest_ghost_dist < 900:
-            buster_iter.closest_ghost_x = 0
-            buster_iter.closest_ghost_y = 0
+            buster_iter.closest_ghost_x = buster_iter.closest_ghost_x - ( -900 if my_team_id == 0 else 900)
+            buster_iter.closest_ghost_y = buster_iter.closest_ghost_y - ( -900 if my_team_id == 0 else 900)
             buster_iter.status = "CHASING"
         else:
             buster_iter.status = "IDLE"
 
+def random_mvm(buster: Buster   ):
+    pass
 
 # game loop
 while True:
-    all_buster = []
     all_ghost = []
-    min_buster = []
 
     entities = int(input())  # the number of busters and ghosts visible to you
 
@@ -130,28 +154,27 @@ while True:
 
     print(f"ghost count: {len(all_ghost)} ", file=sys.stderr, flush=True)
     #print(f"buster count: {len(all_buster)}", file=sys.stderr, flush=True)
-    closest_busters(all_ghost, all_buster)
-    update_status(all_buster)
+    closest_busters(all_ghost)
+    update_status()
 
     #for i in range(busters_per_player):
-    for buster in all_buster:
+    for index, buster in enumerate(all_buster):
         # Write an action using print
         # To debug: print("Debug messages...", file=sys.stderr, flush=True)
         # MOVE x y | BUST id | RELEASE
         print(f"buster {buster.entity_id} closes ghost: {buster.closest_ghost} | status: {buster.status}", file=sys.stderr, flush=True)
+        
         if buster.status == "CHASING":
             print(f"MOVE {buster.closest_ghost_x} {buster.closest_ghost_y}")
 
         elif buster.status == "BUSTING":
             print(f"BUST {buster.closest_ghost}")
 
-        elif buster.status == "GB": 
-            print(f"MOVE {0} {0}")
+        elif buster.status == "GB":
+            print(f"MOVE {ori_x} {ori_y}")
 
         elif buster.status == "READY":
             print(f"RELEASE")
 
         else:
-            print(
-                f"MOVE {int(math.tan(math.radians((buster.entity_id+1)*(90/(busters_per_player+1))))*9000)} 9000"
-            )
+            print(f"MOVE {int(16000/2)} {int(9000/2)}")
